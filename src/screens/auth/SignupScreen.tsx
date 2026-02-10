c47501171be88d37030ebd0f7a311a239858007c
dani0910 <dlekdms0910@naver.com>
2026-02-11 02:07:47 +0900

    feat : [회원가입] 약관 동의 화면 ui 및 레이아웃 구성

--- src/screens/auth/SignupScreen.tsx
+++ src/screens/auth/SignupScreen.tsx
@@ -20,12 +20,51 @@ export function SignupScreen({ navigation }: Props){
                 <Text style={styles.title}>
                     서비스 이용 약관에{'\n'}동의해주세요
                 </Text>
-            </View>
 
-            <View style={styles.agreementSection}>
-                <View style={styles.allAgreeRowPlaceholder}></View>
-                <View style={styles.divider}></View>
-                <View style={styles.agreementListPlaceholder}></View>
+                <View style={styles.agreementSection}>
+                    {/* 모두 동의 */}
+                    <View style={styles.allAgreeRowPlaceholder}>
+                        <View style={styles.circle}/>
+                        <Text style={styles.rowText}>네, 모두 동의합니다</Text>
+                    </View>
+
+                    <View style={styles.divider}></View>
+
+                    {/* 개별 약관 */}
+                    <View style={styles.agreementRowWrap}>
+                        <View style={styles.rowLeft}>
+                            <View style={styles.circle}/>
+                            <Text style={styles.rowText}>(필수) 서비스 이용약관에 동의</Text>
+                        </View>
+                        <Text style={styles.viewText}>보기</Text>
+                    </View>
+
+                    <View style={styles.agreementRowWrap}>
+                        <View style={styles.rowLeft}>
+                            <View style={styles.circle}/>
+                            <Text style={styles.rowText}>(필수) 개인정보 처리방침에 동의</Text>
+                        </View>
+                        <Text style={styles.viewText}>보기</Text>
+                    </View>
+
+                    <View style={styles.agreementRowWrap}>
+                        <View style={styles.rowLeft}>
+                            <View style={styles.circle}/>
+                            <Text style={styles.rowText}>(필수) 만 14세 이상입니다</Text>
+                        </View>
+                    </View>
+
+                    <View style={styles.agreementRowWrap}>
+                        <View style={styles.rowLeft}>
+                            <View style={styles.circle}/>
+                            <Text style={styles.rowText}>(선택) 마케팅 정보 수신에 동의</Text>
+                        </View>
+                        <Text style={styles.viewText}>보기</Text>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.bottom}>
+                <Button title="다음" onPress={()=>{}}></Button>
             </View>
         </SafeAreaView>
     );
@@ -39,24 +78,62 @@ const styles = StyleSheet.create({
     content : {
         flex: 1,
         paddingHorizontal: 20,
-        paddingTop: 28,
+        // paddingTop: 28,
     },
     title : {
         fontSize: FONT_SIZES.heading1,
         fontFamily: FONTS.pretendard.extraBold,
         color: COLORS.black['500'],
+        marginVertical: 55,
     },
     agreementSection : {
-        marginTop: 24,
+        // marginTop: 24,
     },
     allAgreeRowPlaceholder : {
-
+        flexDirection: 'row',
+        alignItems: 'center',
     },
+    circle : {
+        width: 20,
+        height: 20,
+        borderRadius: 10,
+        borderWidth: 1.5,
+        borderColor: COLORS.gray[500],
+        backgroundColor: 'transparent',
+    },
+    rowText : {
+        marginLeft: 10,
+        fontSize : FONT_SIZES.body1,
+        fontFamily : FONTS.pretendard.regular,
+        color: COLORS.black[500],
+    },
+
     divider : {
-
+        height: 1,
+        backgroundColor: COLORS.gray[500],
+        marginVertical: 24,
     },
-    agreementListPlaceholder : {
 
+    agreementRowWrap : {
+        // backgroundColor: '#ededed',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 24,
     },
+    rowLeft : {
+        flex: 1,
+        flexDirection : 'row',
+        alignItems: 'center',
+    },
+    viewText : {
+        fontSize: FONT_SIZES.body4,
+        fontFamily: FONTS.pretendard.regular,
+        color: COLORS.gray[700],
+    },
+    
+    bottom:{
+        paddingHorizontal: 16,
+        paddingVertical: 10,
+    }
 
 })
